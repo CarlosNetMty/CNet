741f2d0f9b5c6435ac3d0f3ef068b992593fcb9f
Carlos Martinez <carlos.net@live.com>
2014-02-04 17:10:17 -0600

    PoC Draft

---
+++ CNetSolution/CNet.PoC/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+
+using AutoMapper;
+
+namespace CNet.PoC
+{
+    class Item { public string Name { get; set; } }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Staring");
+            Console.ReadLine();
+
+            Mapper.CreateMap<SqlDataReader, Item>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(source => source["LName"]));
+
+
+            string connectionString = "data source=devdb5.maintenancenet.net\\dev;initial catalog=MnetReportUP;User=mn; Password=3int3ll3kt!";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand() 
+            { 
+                Connection = connection,
+                CommandType = System.Data.CommandType.Text,
+                CommandText = "SELECT TOP 10 * FROM tblUsers"
+            };
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            List<Item> result = new List<Item>();
+
+            while (reader.Read()) 
+            { 
+                result.Add(Mapper.Map<Item>(reader));
+            }
+
+            //DataSet dataSet = new DataSet();
+            //SqlDataAdapter adapter = new SqlDataAdapter(command);
+            //adapter.Fill(dataSet);
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<int> collection = new Queue<int>();
+
+            collection.Enqueue(1);
+            collection.Enqueue(2);
+            collection.Enqueue(3);
+
+            Console.WriteLine(collection.Dequeue());
+            Console.ReadLine();
+        }
+    }
+}
